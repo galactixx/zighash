637df9622c31bf8ef5696212ccb5c006e7657fb5
galactixx <nwlezien.github@gmail.com>
2025-05-29 13:02:44 -0500

    adjust build.zig to add library

--- build.zig
+++ build.zig
@@ -3,6 +3,21 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const root_source_file = "src/lib.zig";
 
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const zh_mod = b.addModule("zighash", .{
+        .root_source_file = b.path(root_source_file),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    _ = b.addLibrary(.{
+        .name = "zighash",
+        .linkage = .static,
+        .root_module = zh_mod,
+    });
+
     const unit_tests = b.addTest(.{
         .root_source_file = b.path(root_source_file),
     });
@@ -11,13 +26,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
-
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    _ = b.addModule("zighash", .{
-        .root_source_file = b.path(root_source_file),
-        .target = target,
-        .optimize = optimize,
-    });
 }
